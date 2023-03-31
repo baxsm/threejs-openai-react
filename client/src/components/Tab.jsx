cb9c9e8395ee879634cc7f975710cda38272f1c9
Bobby Aleem <bobbybeatmaker@hotmail.com>
2023-03-31 22:47:59 +0500

    Shirt, background update

---
+++ client/src/components/Tab.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
+
+const Tab = () => {
+
+  const snap = useSnapshot(state)
+
+  return (
+    <div>
+      Tab
+    </div>
+  )
+}
+
+export default Tab
